f2f78c49559430a1f64a14fcd5dc69543b363518
satyajit <xinus404@gmail.com>
2020-06-02 22:28:32 +0530

    Delete Node without head pointer

---
+++ deleteNodeFromLinkedList.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        
+        if(node->next == NULL){
+            // node = NULL;
+            delete node;
+            return ;
+        }
+        
+        node->val = node->next->val;
+        
+        ListNode *copy = node->next;
+        
+        node->next = node->next->next;
+        
+        delete copy;
+        
+    }
+};
